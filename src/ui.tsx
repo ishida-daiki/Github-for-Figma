65dbb3468c2bdc11faa74420df42e3376926ae05
ishida-daiki <ishida-daiki@dmm.com>
2024-07-19 20:14:38 +0900

    Github の環境追加

--- src/ui.tsx
+++ src/ui.tsx
@@ -1,58 +1,55 @@
-import '!prismjs/themes/prism.css'
-
+import React from "react";
+import { BaseStyles, Button, ThemeProvider } from "@primer/react";
 import {
-  Button,
-  Container,
-  render,
-  VerticalSpace
-} from '@create-figma-plugin/ui'
-import { emit } from '@create-figma-plugin/utilities'
-import { h, RefObject } from 'preact'
-import { useCallback, useEffect, useRef, useState } from 'preact/hooks'
-import { highlight, languages } from 'prismjs'
-import Editor from 'react-simple-code-editor'
+  UploadIcon,
+  DownloadIcon,
+  GitBranchIcon,
+  TriangleDownIcon,
+} from "@primer/octicons-react";
+import { render } from "@create-figma-plugin/ui";
+import { emit } from "@create-figma-plugin/utilities";
+import { h, RefObject } from "preact";
+import { useCallback, useEffect, useRef, useState } from "preact/hooks";
+import styles from "./styles.css";
+import { InsertCodeHandler } from "./types";
 
-import styles from './styles.css'
-import { InsertCodeHandler } from './types'
+import "@primer/primitives/dist/css/base/size/size.css";
+import "@primer/primitives/dist/css/base/typography/typography.css";
+import "@primer/primitives/dist/css/functional/size/border.css";
+import "@primer/primitives/dist/css/functional/size/breakpoints.css";
+import "@primer/primitives/dist/css/functional/size/size-coarse.css";
+import "@primer/primitives/dist/css/functional/size/size-fine.css";
+import "@primer/primitives/dist/css/functional/size/size.css";
+import "@primer/primitives/dist/css/functional/size/viewport.css";
+import "@primer/primitives/dist/css/functional/typography/typography.css";
+import "@primer/primitives/dist/css/functional/themes/light.css";
+import "@primer/primitives/dist/css/functional/themes/light-tritanopia.css";
+import "@primer/primitives/dist/css/functional/themes/light-high-contrast.css";
+import "@primer/primitives/dist/css/functional/themes/light-colorblind.css";
+import "@primer/primitives/dist/css/functional/themes/dark.css";
+import "@primer/primitives/dist/css/functional/themes/dark-colorblind.css";
+import "@primer/primitives/dist/css/functional/themes/dark-dimmed.css";
+import "@primer/primitives/dist/css/functional/themes/dark-high-contrast.css";
+import "@primer/primitives/dist/css/functional/themes/dark-tritanopia.css";
 
 function Plugin() {
-  const [code, setCode] = useState(`function add(a, b) {\n  return a + b;\n}`)
-  const containerElementRef : RefObject<HTMLDivElement> = useRef(null)
-  const handleInsertCodeButtonClick = useCallback(
-    function () {
-      emit<InsertCodeHandler>('INSERT_CODE', code)
-    },
-    [code]
-  )
-  // Patch to make `react-simple-code-editor` compatible with Preact
-  useEffect(function () {
-    const containerElement = containerElementRef.current
-    if (containerElement === null) {
-      return
-    }
-    const textAreaElement = containerElement.querySelector('textarea')
-    if (textAreaElement === null) {
-      return
-    }
-    textAreaElement.textContent = code
-    const preElement = containerElement.querySelector('pre')
-    if (preElement === null) {
-      return
-    }
-    if (textAreaElement.nextElementSibling !== preElement) {
-      textAreaElement.after(preElement)
-    }
-  }, [code])
   return (
-    <Container space="medium">
-      <VerticalSpace space="small" />
-      <VerticalSpace space="large" />
-      <Button fullWidth onClick={handleInsertCodeButtonClick}>
-        Insert Code
-      </Button>
-      <VerticalSpace space="small" />
-    </Container>
-  )
+    <ThemeProvider colorMode={"auto"}>
+      <BaseStyles>
+        <div style={{display: 'flex'}}>
+          <Button leadingVisual={GitBranchIcon} trailingAction={TriangleDownIcon}>
+            main
+          </Button>
+          <div className={styles.IconButton}>
+            <UploadIcon/>
+          </div>
+          <div className={styles.IconButton}>
+            <DownloadIcon />
+          </div>
+        </div>
+      </BaseStyles>
+    </ThemeProvider>
+  );
 }
 
-export default render(Plugin)
+export default render(Plugin);
